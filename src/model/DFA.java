908b0342280430b10ac68e59fce0fffbf044e722
Alex H <hehe3301@gmail.com>
2015-04-20 15:20:44 -0400

    Added comments & debug field to DFA

--- src/model/DFA.java
+++ src/model/DFA.java
@@ -2,41 +2,72 @@ package model;
 
 import java.util.Hashtable;
 
+/**
+ * @author Alex
+ *
+ */
 public class DFA {
 
-	
-	public Hashtable<String,State> states;
+	public Hashtable<String, State> states;
 	public State startState;
-	
-	public DFA(){
-		states= new Hashtable<String,State>();
-	}//DFA
-	
-	public void addTransition(String start,String letter, String end){
-		if(!states.contains(start)){
+
+	public DFA() {
+		states = new Hashtable<String, State>();
+	}// DFA
+
+	/**
+	 * This method adds a transition based on a letter and state to a State
+	 * 
+	 * @param start
+	 *            - the state that the transition is being added to
+	 * @param letter
+	 *            - the letter of the transition
+	 * @param end
+	 *            - the state the machine should be in after the
+	 */
+	public void addTransition(String start, String letter, String end) {
+		if (!states.contains(start)) {
 			states.put(start, new State(start));
-		}//if
-		if(!states.contains(end)){
+		}// if
+		if (!states.contains(end)) {
 			states.put(end, new State(end));
-		}//if
+		}// if
 		states.get(start).addTransition(letter, states.get(end));
 	}
-	
-	public void setStartState(String start){
+
+	/**
+	 * This method sets the start state of the machine
+	 * 
+	 * @param start
+	 *            - the name of the state to be set to start
+	 */
+	public void setStartState(String start) {
+		if (!states.contains(start)) {
+			states.put(start, new State(start));
+		}// if
 		startState = states.get(start);
 	}
-	
-	public boolean parseString(String stringIn){
+
+	/**
+	 * this methoid parses a string using the DFA
+	 * 
+	 * @param stringIn
+	 *            - the string to be parsed
+	 * @param debug
+	 *            - flag if debug messages should be printed
+	 * @return - boolean if the string should be accepted or not
+	 */
+	public boolean parseString(String stringIn, boolean debug) {
 		State current = startState;
 		String toRead = stringIn;
-		while(toRead.length()>1){
-			String head = toRead.substring(0,0);
-			toRead = toRead.substring(1, toRead.length()-1);
-			current=current.getNextState(head);
+		while (toRead.length() > 1) {
+			String head = toRead.substring(0, 0);
+			toRead = toRead.substring(1, toRead.length() - 1);
+			current = current.getNextState(head);
 		}
 		String head = toRead.substring(0);
-		current=current.getNextState(head);
+		current = current.getNextState(head);
 		return current.isAcceptState();
-		
+
 	}
 }
--- src/model/State.java
+++ src/model/State.java
@@ -6,28 +6,64 @@ public class State {
 	private String name;
 	private Hashtable<String, State> transitions = null;
 	private boolean isAcceptState = false;
-	
+
+	/**
+	 * Public constructor for State objects
+	 * 
+	 * @param name
+	 *            - the name of the state created
+	 */
 	public State(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * A method to add or update a transition from this state to another
+	 * 
+	 * @param letter
+	 *            - the character to parse
+	 * @param state
+	 *            - the state the charicter will cause a transition to
+	 */
 	public void addTransition(String letter, State state) {
 		transitions.put(name, state);
 	}
-	
-	public State getNextState(String letter){
+
+	/**
+	 * Method to return the State object of the "next" state based on the letter
+	 * given
+	 * 
+	 * @param letter
+	 *            - the letter to be parsed
+	 * @return - the State object of the state that should be next based on the
+	 *         letter
+	 */
+	public State getNextState(String letter) {
 		return transitions.get(letter);
 	}
 
-	public String getName(){
+	/**
+	 * "Getter" for the name of the state
+	 * 
+	 * @return - String name of the state
+	 */
+	public String getName() {
 		return this.name;
 	}
-	
-	public void setAcceptState(){
+
+	/**
+	 * This method sets the state to be an accepting state
+	 */
+	public void setAcceptState() {
 		isAcceptState = true;
 	}
-	
-	public boolean isAcceptState(){
+
+	/**
+	 * This method is to check to see if this state is an accepting state
+	 * 
+	 * @return
+	 */
+	public boolean isAcceptState() {
 		return isAcceptState;
 	}
 }
